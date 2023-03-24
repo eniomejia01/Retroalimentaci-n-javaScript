9868e53726b8a2c28119695f9cba9b64558f8c64
Enio <eniomejia57@gmail.com>
2023-03-24 16:49:18 -0600

    cambios protip

--- assets/js/protip-retorno.js
+++ assets/js/protip-retorno.js
@@ -17,14 +17,41 @@ const imprimeArgumentos2 = ( edad, ...args) => { // los tres puntos son importan
     // console.log(edad, args );
     return args;
 }
-const argumentos = imprimeArgumentos2(10, true, 'Enio', 'Hola');
-
-console.log({ argumentos });
+const [casado, vivo, nombre, saludo] = imprimeArgumentos2(10, true, 'Enio', 'Hola');
 
 
+console.log({casado, vivo, nombre, saludo});
 
 
+const {apellido: nuevoApellido} = crearPersona('Enio', 'Mejia');
+console.log({nuevoApellido});
 
 
+const tony = {
+
+    nombre: 'Tony Stark',
+    codeName: 'IronMan',
+    vivo: false,
+    edad: 40,
+    trajes: ['Mark I', 'Mark V', 'Hulkbuster'],
+};
+
+// const imprimePropiedades = ( personaje ) => {
+//     console.log( 'nombre',personaje.nombre);
+//     console.log( 'codeName',personaje.codeName);
+//     console.log( 'vivo',personaje.vivo);
+//     console.log( 'edad',personaje.edad);
+//     console.log( 'trajes',personaje.trajes);
+// }
 
 
+const imprimePropiedades = ( {nombre, codeName, vivo, edad, trajes} ) => {
+    console.log({nombre});
+    console.log({codeName});
+    console.log({vivo});
+    console.log({edad});
+    console.log({trajes});
+    
+    
+}
+imprimePropiedades( tony );
